d6fd3684f2a17f6637aa9ab61a9c981ef58ead05
bilalpasta17 <83414545+bilalpasta17@users.noreply.github.com>
2023-06-20 10:45:23 +0500

    Custom_Alert & Default Alert

--- app/src/main/java/com/example/bykeaapplication/activity_home.java
+++ app/src/main/java/com/example/bykeaapplication/activity_home.java
@@ -2,7 +2,12 @@ package com.example.bykeaapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class activity_home extends AppCompatActivity {
 
@@ -11,6 +16,31 @@ public class activity_home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        
+
+//        Dialog dialog=new Dialog(activity_home.this);
+//        dialog.setContentView(R.layout.alert);
+//        dialog.findViewById(R.id.button).setOnClickListener(b->{
+//            Toast.makeText(activity_home.this,"Done",Toast.LENGTH_LONG).show();
+//            dialog.setCancelable(false);
+//            dialog.dismiss();
+//        });
+//        dialog.setCancelable(false);
+//        dialog.show();
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity_home.this);
+        builder.setTitle("Logout");
+        builder.setMessage("Do you want to Logout?");
+        builder.setPositiveButton("YES", (dialogInterface, i) -> {
+            Toast.makeText(this, "Logout Successful", Toast.LENGTH_SHORT).show();
+        });
+        builder.setNegativeButton("No", (dialogInterface, i) -> {
+
+            Toast.makeText(this, "Logout UnSuccessful", Toast.LENGTH_SHORT).show();
+        });
+
+        builder.show();
+
+
     }
 }
